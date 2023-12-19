2b47433da4204bf6d14d79f02a22d767814814db
Edward Molter <emolter@berkeley.edu>
2023-12-19 12:34:32 -0800

    still trying to fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
 # migrated to pyproject.toml
 from sphinx_pyproject import SphinxConfig
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 config = SphinxConfig("../pyproject.toml", globalns=globals())
