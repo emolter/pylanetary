763fa0c0857f9c7ad3d0b2595a9d8912584a4eb1
Sarah Howard <skathryn@halo2.atmos.umd.edu>
2024-02-09 13:54:30 -0500

    add plot_lines method to Spectrum class

--- pylanetary/spectral/core.py
+++ pylanetary/spectral/core.py
@@ -22,34 +22,58 @@ def centered_list(n):
     centered_list = list(range(-half, half + 1,2))
     return centered_list
 
+# Guide to keys in the nested model dictionary:
+# model[] - Model name, taken from user input, string
+# ref - short reference name of model type, string
+# lmfit_func - built-in LMFIT function name, text 
+# label - fit label for plotting - string
+    
+model = {}
+model['Gaussian'] = {'ref': 'gauss', 'lmfit_func': GaussianModel(),'label': 'Gaussian Fit','color':'orange'}
+model['Lorentzian'] = {'ref': 'lor','lmfit_func':LorentzianModel(),'label': 'Lorentzian Fit','color':'green'}
+model['Voigt'] = {'ref': 'voigt','lmfit_func':VoigtModel(),'label': 'Voigt Fit','color':'red'}
+model['PseudoV'] = {'ref': 'psv','lmfit_func':PseudoVoigtModel(),'label': 'Pseudo-Voigt Fit','color':'purple'}
+model['Moffat'] = {'ref': 'mof','lmfit_func':MoffatModel(),'label': 'Moffat Fit','color':'brown'}
+
 ###############################################################################
 ###############################################################################
 
 class Spectrum:
   '''
-  Parameters
-  ----------
-  spec = intensity axis, numpy array of floats (required)
-  rest_f = rest frequency of molecular transition, float (required for fit_profile and calc_doppler_vel) 
-  wl = wavelength axis, optional. One of wl or freq must be specified
-  freq = frequency axis, optional. One of wl or freq must be specified.
-  ...
-  
   Examples
   --------
   Initialize a Spectrum object:
-  your_spec = spectrum(wl,spec,rest_f)
+  your_spec = Spectrum(spec, freq = freq,rest_f = 345.79598990,RMS = 0.017530593)
   
-  Fit the spectrum with a Lorentian model and plot the results:
-  peak, std, = test.fit_profile(fit_type = 'Lorentzian', showplots = 'y', RMS = 0.017530593)
+  Calculate the best fit of the spectrum using a Lorentzian fit:
+  test_peak, test_std, chi, chi_red = test.fit_profile(fit_type = 'Lorentzian')
   
-  Calculate the Doppler velcoity using the calculated Lorentzian model:
-  wind, err = test.calc_doppler_vel(rest_freq = 345.79598990, peak = peak, err = std)
+  Calculate the best fit of the spectrum with all line shapes and save the figure:
+  test_peak, test_std, chi, chi_red = test.fit_profile(fit_type = 'Moffat', plot_name = 'line_comparison.png')
+  
+  Calculate the Doppler shifted winds:
+  test_wind, test_err = test.calc_doppler_vel(peak = test_peak, err = test_std)
   '''
   
   ###############################################################################
   
-  def __init__(self, spec, rest_f, wl = None, freq = None):
+  def __init__(self, spec, rest_f, wl = None, freq = None, RMS = None):
+    '''
+    Parameters
+    ----------
+    spec = intensity axis, numpy array of floats (required)
+    rest_f = rest frequency of molecular transition, float (required for fit_profile and calc_doppler_vel) 
+    wl = wavelength axis, optional. One of wl or freq must be specified
+    freq = frequency axis, optional. One of wl or freq must be specified.
+    RMS (optional) - numerical value of the data's RMS, float. If not specified, an RMS value is estimated through sigma clipping.
+    
+    Calculated Attributes
+    ---------------------
+    wl_res - wavelength resoltuion
+    freq_res - frequency resolution
+    chan_num - number of wavelength/frequency channels
+    
+    '''
     self.wl = wl
     self.freq = freq
 
@@ -58,97 +82,78 @@ class Spectrum:
     elif self.freq is None:
       self.freq = c/self.wl
 
-    self.spec = spec # attribute, spectral inensity axis
-    self.rest_f = rest_f # attribute, rest frequency of the molecular transition
-    self.wl_res = self.wl[1] - self.wl[0] # attribute, wavelength resolution
-    self.freq_res = self.freq[1] - self.freq[0] # attrbute, frequency resolution
-    self.chan_num = np.size(self.wl) # attribute, number of wavelength/frequency channels
+    self.spec = spec
+    self.rest_f = rest_f
+    
+    if RMS is None:
+      self.RMS,median,rms_std = sigma_clipped_stats(self.spec)
+    else:
+      self.RMS = RMS
+      
+    self.wl_res = self.wl[1] - self.wl[0]
+    self.freq_res = self.freq[1] - self.freq[0]
+    self.chan_num = np.size(self.wl)
 	
   ###############################################################################
   
-  def fit_profile(self, fit_type, showplots, initial_fit_guess = None, RMS = None):
+  def fit_profile(self, fit_type, showplots = True, plot_name = None, initial_fit_guess = None):
     '''
+    Calculate the best fit of a Spectral object using the LMFIT algorithm. The line shapes can be plotted for visual comparison.
+    
     Parameters
     ----------
     fit_type - the type of line shape used to fit the spectra, string. Options are:
-        All: Fit the spectrum with all line 5 line shape options and plot for visual comparison. Data is always plotted
+        All: Fit the spectrum with all line 5 line shape options. Data is always plotted.
         Gaussian: fit the spectrum with a Gaussian function
         Lorentzian: fit the spectrum with a Lorentzian function
         Voigt: fit the spectrum with a Voigt function
         PseudoV: fit the spectrum with a Pseudo-Voigt function
         Moffat: fit the spectrum with a Moffat function
         A description of all line shapes can be found at https://lmfit.github.io/lmfit-py/builtin_models.html
-    showplots - indicator of whether to plot or not, string - 'y' or 'Y' will plot the fit results
-
-    initial_fit_guess (optional) - path to a saved LMFIT result to use as the initial guess. This can be beneficial if some spectra are very noisy. It is recommended that you use a pixel with as small a Doppler shift as possible to make your initial guess.  
-    RMS (optional) - numerical value of the data's RMS, float
+        
+    showplots - indicator of whether to plot. Default is True.
+    plot_name - name of the saved figure, optional. Default is None.
+    initial_fit_guess (optional) - path to a saved LMFIT result to use as the initial guess. This can be beneficial if some spectra are very noisy. It is recommended that you use a pixel with as small a Doppler shift as possible to make your initial guess. Default is None.
+    
+    Returns
+    -------
+    peak - frequency or wavelength value of the line peak, LMFIT's 'center' parameter.
+    err - standard deviation of the frequency or wavelength value.
+    chi - calculated Chi-squared statistic of the line fit.
+    chi_red - calculated reduced Chi-squared statistic of the line fit.
     '''
     
-    if RMS == None:
-      RMS,median,rms_std = sigma_clipped_stats(self.spec)
-      
-    # Guide to keys in the nested model dictionary:
-    # model[] - Model name, taken from user input, string
-    # ref - short reference name of model type, string
-    # lmfit_func - built-in LMFIT function name, text 
-    # label - fit label for plotting - string
-    
-    model = {}
-    model['Gaussian'] = {'ref': 'gauss', 'lmfit_func': GaussianModel(),'label': 'Gaussian Fit','color':'orange'}
-    model['Lorentzian'] = {'ref': 'lor','lmfit_func':LorentzianModel(),'label': 'Lorentzian Fit','color':'green'}
-    model['Voigt'] = {'ref': 'voigt','lmfit_func':VoigtModel(),'label': 'Voigt Fit','color':'red'}
-    model['PseudoV'] = {'ref': 'psv','lmfit_func':PseudoVoigtModel(),'label': 'Pseudo-Voigt Fit','color':'purple'}
-    model['Moffat'] = {'ref': 'mof','lmfit_func':MoffatModel(),'label': 'Moffat Fit','color':'brown'}
-   
     if fit_type != 'All':
       if initial_fit_guess == None: # If no initial guess is given, calculate one using the built-in LMFIT .guess method
         pars = model[fit_type]['lmfit_func'].guess(self.spec,x=self.freq)
       else: # Load initial guess if parameter is provided
         inital = load_modelresult(initial_fit_guess) # Inital guess that is best fit from center pixel
         pars = inital.params
-
       fit_result = model[fit_type]['lmfit_func'].fit(self.spec,pars,x=self.freq,scale_covar=False)
       print(fit_result.fit_report()) # Print out the best fit parameters and statistics
     elif fit_type == 'All':
-      showplots = 'n'
-      initial_fit_guess = None
-      f, (a0,a1) = plt.subplots(2,1,height_ratios = [3,1])
-      a0.vlines(x = self.rest_f, ymin=0, ymax=np.max(self.spec), color = 'grey', linestyles='dashed', label='Rest Frequency')
-      a0.errorbar(self.freq,self.spec,yerr=RMS, fmt='o',label='Original Data')
+      fit_result = {}
       for key in model:
         pars = model[key]['lmfit_func'].guess(self.spec,x=self.freq)
         result = model[key]['lmfit_func'].fit(self.spec,pars,x=self.freq,scale_covar=False)
-        residuals = self.spec - result.best_fit
         print(result.fit_report())
-        # Plot the results
-        a0.plot(self.freq,result.best_fit,'-',label = model[key]['label'],color = model[key]['color'])
-        a1.plot(self.freq,residuals,'--',color = model[key]['color'])
-        # End of for loop
-      a1.set_xlabel('Frequency (GHz)')
-      a0.set_ylabel('Intensity (mJy)')
-      a1.set_ylabel('Residuals')
-      a0.title.set_text('Comparison of Line Shapes for a Pressure Broadened Atmosphere')
-      a0.legend()
-      plt.show()
+        fit_result[key] = result
     else:
       raise ValueError('fit_type must be one of the following options: Gaussian, Lorentzian, Voigt, PseudoV, Moffat, or All')
     
     # Calculate statistics manually for single fit type
     if fit_type != 'All':
-      chi = np.sum(((np.subtract(self.spec,fit_result.best_fit)/RMS))**2)
+      chi = np.sum(((np.subtract(self.spec,fit_result.best_fit)/self.RMS))**2)
       chi_red = chi/(self.chan_num - 4)
     else:
       chi = np.NAN
       chi_red = np.NAN
 
-    if showplots == 'y' or showplots == 'Y':
-      plt.vlines(x = self.rest_f, ymin=0, ymax=np.max(self.spec), color = 'grey', linestyles='dashed', label='Rest Frequency')
-      plt.errorbar(self.freq,self.spec,yerr=RMS, fmt='o',label='Original Data')
-      plt.plot(self.freq,fit_result.best_fit,'-',label = model[fit_type]['label'])
-      plt.xlabel('Frequency (GHz)')
-      plt.ylabel('Intensity (mJy)')
-      plt.legend()
-      plt.show()
+    if showplots is True:
+      test.plot_lines(line_result = fit_result, model_key = fit_type)
+    
+    if plot_name is not None:
+      plt.savefig(plot_name)
     
     if fit_type != 'All':
       peak = fit_result.params['center']
@@ -161,12 +166,58 @@ class Spectrum:
     
   ############################################################################### 
   
-  def calc_doppler_vel(self, peak, err): #, observed_freq_uncertainty=0.0):
+  def plot_lines(self, line_result, model_key, x_label = 'Frequency (GHz)', y_label = 'Intensity (mJy)', residual_label = 'Residuals', plot_title = 'Comparison of Line Shapes'):
     '''
+    Plot the original data and best fit line shape for visual comparison.
+    
+    Parameters
+    ----------
+    line_result - the LMFIT best fit result.
+    model_key - the name of the line shape used to calculate the fit.
+    
+    x_label - x axis label for the main and residual plot, optional. Default is 'Frequency (GHz)'.
+    y_label - y axis label for the main plot, optional. Default is 'Intensity (mJy)'.
+    residual_label - y axis label for the residual plot, optional. Default is 'Residuals'.
+    plot_title - title for the plot, optional. Default is 'Comparison of Line shapes'.
+    
+    '''
+    
+    f, (a0,a1) = plt.subplots(2,1,height_ratios = [3,1])
+    a0.vlines(x = self.rest_f, ymin=0, ymax=np.max(self.spec), color = 'grey', linestyles='dashed', label='Rest Frequency')
+    a0.errorbar(self.freq, self.spec, yerr=self.RMS, fmt='o', label='Original Data')
+    
+    if model_key == 'All':
+      for key in model:
+        residuals = self.spec - line_result[key].best_fit
+        a0.plot(self.freq,line_result[key].best_fit,'-',label = model[key]['label'],color = model[key]['color'])
+        a1.plot(self.freq,residuals,'--',color = model[key]['color'])
+    else:
+      residuals = self.spec - line_result.best_fit
+      a0.plot(self.freq,line_result.best_fit,'-',label = model[model_key]['label'],color = model[model_key]['color'])
+      a1.plot(self.freq,residuals,'--',color = model[model_key]['color'])
+    
+    a1.set_xlabel(x_label)
+    a0.set_ylabel(y_label)
+    a1.set_ylabel(residual_label)
+    a0.title.set_text(plot_title)
+    a0.legend()
+    plt.show()
+    
+  ###############################################################################
+  
+  def calc_doppler_vel(self, peak, err):
+    '''
+    Calculate the Doppler velocity from the calculated center frequency.
+    
     Parameters
     ---------- 
     peak - the calculated central peak frequency of the spectral line peak, float
     err - the standard deviation of the calculated central peak frequency, float
+    
+    Returns
+    -------
+    v - the calculated Doppler velocity, m/s.
+    wind_err - the Doppler velocity error, calculated through error propagation, m/s.
     '''
 
     peak = peak
@@ -175,10 +226,10 @@ class Spectrum:
     # Doppler equation
     v = ((-1*peak/self.rest_f)+1)*c # Calculate Doppler shifted winds
     print('The wind speed is '+str(v)+'m/s')
-    err = np.abs(a*std) # Calculate the error in velocity
-    print('The wind error is '+str(err)+'m/s')
+    wind_err = np.abs(a*std) # Calculate the error in velocity
+    print('The wind error is '+str(wind_err)+'m/s')
     
-    return v, err
+    return v, wind_err
     
   ###############################################################################
    
@@ -190,9 +241,9 @@ class Spectrum:
   #freq = np.array([345.76770566,345.76868215,345.76965863,345.77063511,345.77161159,345.77258807,345.77356455,345.77454103,345.77551751,345.77649399,345.77747047,345.77844695,345.77942343,345.78039991,345.78137639,345.78235287,345.78332935,345.78430584,345.78528232,345.7862588,345.78723528,345.78821176,345.78918824,345.79016472,345.7911412,345.79211768,345.79309416,345.79407064,345.79504712,345.7960236,345.79700008,345.79797656,345.79895304,345.79992953,345.80090601,345.80188249,345.80285897,345.80383545,345.80481193,345.80578841,345.80676489,345.80774137,345.80871785,345.80969433,345.81067081,345.81164729,345.81262377,345.81360025,345.81457673,345.81555322,345.8165297,345.81750618,345.81848266,345.81945914,345.82043562,345.8214121,345.82238858])
   #spec = np.array([0.21198112,0.18924561,0.21650964,0.22294408,0.24960007,0.22984277,0.25219446,0.24985315,0.25343025,0.28705364,0.3061584,0.31352046,0.3277193,0.3265509,0.3436137,0.35133913,0.38450843,0.4165357,0.43355167,0.467518,0.49524495,0.5277506,0.5910206,0.64392966,0.7271694,0.89640355,0.8984462,0.76777405,0.6531943,0.59134954,0.55332845,0.50916755,0.46389887,0.4410508,0.4036594,0.40885097,0.37320843,0.3508784,0.34755984,0.31414196,0.29820374,0.27975425,0.28136522,0.26828533,0.26605716,0.24770543,0.2537956,0.22927019,0.219696,0.21160068,0.20004132,0.1939475,0.18222019,0.16944197,0.17536835,0.16506103,0.16306329])
   
-  #test = Spectrum(spec, freq = freq,rest_f = 345.79598990)
+  #test = Spectrum(spec, freq = freq,rest_f = 345.79598990,RMS = 0.017530593)
   #test = Spectrum(spec, freq = freq) #HCN
-  #test_peak, test_std, chi, chi_red = test.fit_profile(fit_type = 'Moffat', showplots = 'y')
+  #test_peak, test_std, chi, chi_red = test.fit_profile(fit_type = 'Moffat')
   #test_wind, test_err = test.calc_doppler_vel(peak = test_peak, err = test_std)
   #test_wind, test_err = test.calc_doppler_vel(rest_freq = 354.50547790, peak = test_peak, err = test_std) # HCN
 
@@ -644,6 +695,7 @@ class SpectralCube:
   ###############################################################################
     
 # Testing case for image cube analysis
+'''
 if __name__=="__main__":
   image = '/homes/metogra/skathryn/Research/Data/ContSub/CO/CS20/Neptune_Pri_X50a4_CS20_narrow_square_2.fits'
   #image = '/homes/metogra/skathryn/Research/Data/ContSub/HCN/CS12/Neptune_Pri_X50a4_HCN_CS12_narrow_square_2.fits'
@@ -656,4 +708,4 @@ if __name__=="__main__":
   #test_cube.fit_cube(datas = test_data, fit_type = 'All', xaxis = test_axis, outfile = 'hcn_poster_figure', RMS = 0.017530593, initial_fit_guess = '/homes/metogra/skathryn/Research/Scripts/hcn_moffat_modelresult.sav')
   test_cube.wind_calc(picklefile = '/homes/metogra/skathryn/pylanetary_dev/pylanetary/pylanetary/spectral/development_testing_wind.pickle', restfreq = 345.79598990, outfile = 'development_testing_wind')
   test_cube.plot_data(picklefile = 'development_testing_wind.pickle', platescale = 0.1, spatial = 4e4, dist = 30.4627707075422, planetrad = 24622., subobslat = -26.167088, ccw = -34., bmaj = 0.4416242241859436, bmin = 0.3885720372200012, bpa = 81.32388305664062, title = 'CO Doppler Velocity Map (4/30/16)', cont_label = 'Radial Velocity (m/s)', limits = [-2000,2000], contours = [21,200])
-  
+'''  
